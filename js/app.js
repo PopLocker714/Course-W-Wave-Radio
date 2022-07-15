c96cc1922dbd9146740378a3fd2bc560d54ab8fc
Ilya <jonirootman714@gmail.com>
2022-07-15 22:12:08 +0300

    fix bem header and hero

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ const TRANSMISSIONS_CARDS = document.querySelectorAll('.transmissions__cards')
 const ACCORDION_TABS = document.querySelectorAll('.accordion__tab__item')
 const PRODACTION_ITEMS = document.querySelectorAll('.prodaction__item')
 
-document.querySelector('.product__buy').addEventListener('click', (event) => {event.preventDefault()})
+
 
 // !HEADER PANEL
 let closing_panel = false
@@ -50,162 +50,164 @@ HEADER_BURGER_BTN.addEventListener('click', function () {
   }
 })
 
-// !PODCASTS
-PODCASTS_BTN.addEventListener('click', showPodcastsItems)
-function showPodcastsItems() {
-  const hideItems = document.querySelectorAll('.playlist_hide')
-  if (hideItems.length !== 0) {
-    for (let i = 0; i < hideItems.length; i++) {
-      removeClass(hideItems[i], 'playlist_hide')
-    }
-    PODCASTS_BTN.removeEventListener('click', showPodcastsItems)
-  }
-}
+// document.querySelector('.product__buy').addEventListener('click', (event) => {event.preventDefault()})
 
-// !TRANSMISSIONS
-// select toggle
-TRANSMISSIONS_SELECT.addEventListener('change', event => {
-  switch (event.detail.value) {
-    case 'Дмитрий Гутенберг':
-      choiceActive(TRANSMISSIONS_CARDS, 0, 'active')
-      break
-    case 'Татьяна Флеганова':
-      choiceActive(TRANSMISSIONS_CARDS, 1, 'active')
-      break
-    case 'Анна Васильева':
-      choiceActive(TRANSMISSIONS_CARDS, 2, 'active')
-      break
-    case 'Пётр Дмитриевский':
-      choiceActive(TRANSMISSIONS_CARDS, 3, 'active')
-      break
-    default:
-      choiceActive(TRANSMISSIONS_CARDS, 0, 'active')
-      break
-  }
-})
+// // !PODCASTS
+// PODCASTS_BTN.addEventListener('click', showPodcastsItems)
+// function showPodcastsItems() {
+//   const hideItems = document.querySelectorAll('.playlist_hide')
+//   if (hideItems.length !== 0) {
+//     for (let i = 0; i < hideItems.length; i++) {
+//       removeClass(hideItems[i], 'playlist_hide')
+//     }
+//     PODCASTS_BTN.removeEventListener('click', showPodcastsItems)
+//   }
+// }
 
-// !GUEST
-// buttns toggle
+// // !TRANSMISSIONS
+// // select toggle
+// TRANSMISSIONS_SELECT.addEventListener('change', event => {
+//   switch (event.detail.value) {
+//     case 'Дмитрий Гутенберг':
+//       choiceActive(TRANSMISSIONS_CARDS, 0, 'active')
+//       break
+//     case 'Татьяна Флеганова':
+//       choiceActive(TRANSMISSIONS_CARDS, 1, 'active')
+//       break
+//     case 'Анна Васильева':
+//       choiceActive(TRANSMISSIONS_CARDS, 2, 'active')
+//       break
+//     case 'Пётр Дмитриевский':
+//       choiceActive(TRANSMISSIONS_CARDS, 3, 'active')
+//       break
+//     default:
+//       choiceActive(TRANSMISSIONS_CARDS, 0, 'active')
+//       break
+//   }
+// })
 
-// ACCORDION_TABS
-ACCORDION_TABS.forEach(el => el.addEventListener('click', accordionTabsHandler))
-function accordionTabsHandler() {
-  ACCORDION_TABS.forEach(el => removeClass(el, 'active'))
-  addClass(this, 'active')
+// // !GUEST
+// // buttns toggle
 
-  PRODACTION_ITEMS.forEach(el => removeClass(el, 'active'))
-  PRODACTION_ITEMS.forEach(el => {
-    if(+el.dataset.prodactionId === +this.dataset.accordionId) {
-      addClass(el, 'active')
-    }
-  })
-}
+// // ACCORDION_TABS
+// ACCORDION_TABS.forEach(el => el.addEventListener('click', accordionTabsHandler))
+// function accordionTabsHandler() {
+//   ACCORDION_TABS.forEach(el => removeClass(el, 'active'))
+//   addClass(this, 'active')
 
-
-
-
-
-updateAPI()
-function updateAPI() {
-  const choices = new Choices(TRANSMISSIONS_SELECT, {
-    searchEnabled: false,
-    itemSelectText: '',
-    position: 'bottom',
-    shouldSort: false,
-    placeholder: false,
-    // renderChoiceLimit: 4,
-  })
-  // accordion ju
-  $("#accordion").accordion({
-    active: 0,
-    collapsible: true,
-
-    icons: {
-      "header": false,
-      "activeHeader": false
-    }
-  })
-}
+//   PRODACTION_ITEMS.forEach(el => removeClass(el, 'active'))
+//   PRODACTION_ITEMS.forEach(el => {
+//     if(+el.dataset.prodactionId === +this.dataset.accordionId) {
+//       addClass(el, 'active')
+//     }
+//   })
+// }
 
 
-// !PLAYLISTS
-
-window.addEventListener('resize', checkSreenSize, false)
-// Playlists Initialize Swiper
-const SWIPER_ITEMS = Array.from(document.querySelectorAll('.playlists__tab__swiper__item'))
-const ARR_SLIDERS_PLAYLISTS = SWIPER_ITEMS.map(el => {
-  return new Swiper(el, {
-    cssMode: true,
-    pagination: {
-      el: ".swiper-pagination",
-      type: "fraction",
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    }
-  })
-})
 
 
-const PLAYLISTS_TABS = Array.from(document.querySelectorAll('.playlists__tab__item'))
-const PLAYLISTS_TABS_MOBILE = Array.from(document.querySelectorAll('.playlist__tab__genre'))
 
-PLAYLISTS_TABS.forEach(el => el.addEventListener('click', playlistsTabsHandler))
-PLAYLISTS_TABS_MOBILE.forEach(el => el.addEventListener('click', playlistsTabsHandler))
-function playlistsTabsHandler() {
-  const item = this
+// updateAPI()
+// function updateAPI() {
+//   const choices = new Choices(TRANSMISSIONS_SELECT, {
+//     searchEnabled: false,
+//     itemSelectText: '',
+//     position: 'bottom',
+//     shouldSort: false,
+//     placeholder: false,
+//     // renderChoiceLimit: 4,
+//   })
+//   // accordion ju
+//   $("#accordion").accordion({
+//     active: 0,
+//     collapsible: true,
+
+//     icons: {
+//       "header": false,
+//       "activeHeader": false
+//     }
+//   })
+// }
+
+
+// // !PLAYLISTS
+
+// window.addEventListener('resize', checkSreenSize, false)
+// // Playlists Initialize Swiper
+// const SWIPER_ITEMS = Array.from(document.querySelectorAll('.playlists__tab__swiper__item'))
+// const ARR_SLIDERS_PLAYLISTS = SWIPER_ITEMS.map(el => {
+//   return new Swiper(el, {
+//     cssMode: true,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       type: "fraction",
+//     },
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     }
+//   })
+// })
+
+
+// const PLAYLISTS_TABS = Array.from(document.querySelectorAll('.playlists__tab__item'))
+// const PLAYLISTS_TABS_MOBILE = Array.from(document.querySelectorAll('.playlist__tab__genre'))
+
+// PLAYLISTS_TABS.forEach(el => el.addEventListener('click', playlistsTabsHandler))
+// PLAYLISTS_TABS_MOBILE.forEach(el => el.addEventListener('click', playlistsTabsHandler))
+// function playlistsTabsHandler() {
+//   const item = this
   
-  PLAYLISTS_TABS.forEach(el => removeClass(el, 'active'))
-  PLAYLISTS_TABS_MOBILE.forEach(el => removeClass(el, 'active'))
-  ARR_SLIDERS_PLAYLISTS.forEach(el => {
-    removeClass(el.el, 'active')
-    if(+item.dataset.targetNumber === +el.el.dataset.number) {
-      addClass(item, 'active')
-      addClass(el.el, 'active')
-    }
-  })
-}
+//   PLAYLISTS_TABS.forEach(el => removeClass(el, 'active'))
+//   PLAYLISTS_TABS_MOBILE.forEach(el => removeClass(el, 'active'))
+//   ARR_SLIDERS_PLAYLISTS.forEach(el => {
+//     removeClass(el.el, 'active')
+//     if(+item.dataset.targetNumber === +el.el.dataset.number) {
+//       addClass(item, 'active')
+//       addClass(el.el, 'active')
+//     }
+//   })
+// }
 
-// !Product Initialize Swiper
+// // !Product Initialize Swiper
 
-var swiperProduct = new Swiper(".product__swiper", {
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-})
+// var swiperProduct = new Swiper(".product__swiper", {
+//   loop: true,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// })
 
 
-function checkSreenSize() {
-  if (innerWidth < 1249) {
-    ARR_SLIDERS_PLAYLISTS.forEach(el => el.navigation.destroy())
-  } else {
-    ARR_SLIDERS_PLAYLISTS.forEach(el => el.navigation.init())
-  }
-  if (innerWidth < 1000) {
-    swiperProduct.pagination.destroy()
-  } else {
-    swiperProduct.pagination.init()
-  }
+// function checkSreenSize() {
+//   if (innerWidth < 1249) {
+//     ARR_SLIDERS_PLAYLISTS.forEach(el => el.navigation.destroy())
+//   } else {
+//     ARR_SLIDERS_PLAYLISTS.forEach(el => el.navigation.init())
+//   }
+//   if (innerWidth < 1000) {
+//     swiperProduct.pagination.destroy()
+//   } else {
+//     swiperProduct.pagination.init()
+//   }
 
-  if(innerWidth < 580) {
-    // accordion ju
-    $("#accordion").accordion({
-      active: 1,
-    })
-  }
-}
-checkSreenSize()
+//   if(innerWidth < 580) {
+//     // accordion ju
+//     $("#accordion").accordion({
+//       active: 1,
+//     })
+//   }
+// }
+// checkSreenSize()
 
-// functions
-function choiceActive(arr, target, className) {
-  for (let i = 0; i < arr.length; i++) {
-    removeClass(arr[i], className)
-  }
-  addClass(arr[target], className)
-}
+// // functions
+// function choiceActive(arr, target, className) {
+//   for (let i = 0; i < arr.length; i++) {
+//     removeClass(arr[i], className)
+//   }
+//   addClass(arr[target], className)
+// }
 
 function addClass(el, targetClass) {
   el.classList.add(targetClass)
