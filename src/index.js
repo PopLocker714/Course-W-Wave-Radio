d129afeef22b02b7c4c03339b29ee33d7eff7288
Ilya <jonirootman714@gmail.com>
2022-10-03 18:06:52 +0300

    add footer + adaptive

--- src/index.js
+++ src/index.js
@@ -27,6 +27,7 @@ import guestHtml from './components/_guest/guest.html'
 import playlistHtml from './components/_playlist/playlist.html'
 import productHtml from './components/_product/product.html'
 import aboutusHtml from './components/_aboutus/aboutus.html'
+import footerHtml from './components/_footer/footer.html'
 
 import loadSection from './modules/loadSection'
 import BurgerMenu from './components/burger/BurgerMenu'
@@ -46,6 +47,7 @@ loadSection(guestHtml, 'section', document.body)
 loadSection(playlistHtml, 'section', document.body)
 loadSection(productHtml, 'section', document.body)
 loadSection(aboutusHtml, 'section', document.body)
+loadSection(footerHtml, 'footer', document.body)
 
 // header
 const burger = new BurgerMenu({
